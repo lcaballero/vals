5feb874d58246e544a805252b60c95696a4313bb
lcaballero <lucas.caballero@logrhythm.com>
2015-09-21 14:48:10 -0600

    Updated code comments.

--- value.go
+++ value.go
@@ -7,6 +7,9 @@ import (
 )
 
 
+// A value instance provides an API over generic data.  The functions
+// At() and In() push new Values onto the end of the chain, drilling
+// down to the desired value.
 type Value struct {
 	key interface{}
 	data interface{}
@@ -14,53 +17,55 @@ type Value struct {
 	err error
 }
 
+// Creates a new instance with the given 'backing' value.
 func New(m interface{}) *Value {
 	return &Value{
 		data: m,
 	}
 }
 
-func (v *Value) Val(key string) interface{} {
+// Attempts to access the value as though it is a map using the given key.
+// If the backing data is not a map this method returns nil.
+func (v *Value) val(key string) interface{} {
 	mp, ok := v.data.(map[string]interface{})
-	if !ok {
+	if ok {
+		return mp[key]
+	} else {
 		v.err = errors.New("Couldn't assert data is map[string]interface{}")
 		return nil
-	} else {
-		return mp[key]
 	}
 }
 
-func (v *Value) At(k string) *Value {
-	return &Value {
-		key: k,
-		data: v.Val(k),
-		previous: v,
-	}
-}
-
+// Access the backing data which this value now contains.
 func (v *Value) Data() interface{} {
 	return v.data
 }
 
+// Returns true if the backing data is non-nil
 func (v *Value) HasValue() bool {
 	return v.data != nil
 }
 
+// Returns true iff backing data is non-nil and it is an array.
 func (v *Value) IsArray() bool {
 	t := reflect.TypeOf(v.data)
 	return t != nil && t.Kind() == reflect.Array
 }
 
+// Returns true iff backing data is non-nil and it is an slice.
 func (v *Value) IsSlice() bool {
 	t := reflect.TypeOf(v.data)
 	return t != nil && t.Kind() == reflect.Slice
 }
 
+// Returns true iff backing data is non-nil and it is an map.
 func (v *Value) IsMap() bool {
 	t := reflect.TypeOf(v.data)
 	return t != nil && t.Kind() == reflect.Map
 }
 
+// Provides the value at the given index as though the backing
+// data is an array or slice (nil if the data is not a slice or array).
 func (v *Value) to(n int) interface{} {
 	var data interface{} = nil
 	if v.IsArray() || v.IsSlice() {
@@ -72,6 +77,8 @@ func (v *Value) to(n int) interface{} {
 	return data
 }
 
+// If data is an array or slice then this function returns the
+// size of the array or slice, otherwise -1
 func (v *Value) Len() int {
 	if v.IsArray() || v.IsSlice() {
 		actual := reflect.ValueOf(v.data)
@@ -80,6 +87,8 @@ func (v *Value) Len() int {
 	return -1
 }
 
+// Saves the index, and loads the value for that index as the backing store
+// if possible.  If the index doesn't exist the new data will be nil.
 func (v *Value) In(n int) *Value {
 	return &Value {
 		key: n,
@@ -88,6 +97,18 @@ func (v *Value) In(n int) *Value {
 	}
 }
 
+// Saves the key, and loads the value for that key from the backing store
+// if possible.  If the key doesn't exist the new Data/value will be nil.
+func (v *Value) At(key string) *Value {
+	return &Value {
+		key: key,
+		data: v.val(key),
+		previous: v,
+	}
+}
+
+// Attempts to cast data to a string, if data cannot be cast to a string
+// this function returns "" else the value as cast to string.
 func (v *Value) AsString() string {
 	s, ok := v.data.(string)
 	if ok {
@@ -98,7 +119,7 @@ func (v *Value) AsString() string {
 	}
 }
 
-// Requires a pointer to fill with Value data
+// Requires a pointer to fill with Value data.
 func (v *Value) Fill(b interface{}) {
 	biz := reflect.ValueOf(b).Elem()
 	n := biz.NumField()
